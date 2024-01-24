240e5f4fffb9184e2d618c209c2c413bd3339a2e
Elsayed Abdullah <sayedaladway@gmail.com>
2024-01-24 21:10:49 +0200

    clicking on pokemon item will open details page for this pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./app.module.css";
+import PokemonList from "./components/PokemonList";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const [pokemonData, setPokemonData] = useState([]);
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
-  const [nextPageUrl, setNextPageUrl] = useState(null);
-  const [prevPageUrl, setPrevPageUrl] = useState(null);
-  const [search, setSearch] = useState("");
+  const [selectedPokemon, setSelectedPokemon] = useState<string | null>(null);
 
-  // to cancel request on cleanup function
-  const controller = new AbortController();
-  const signal = controller.signal;
-
-  const fetchPokemonData = async () => {
-    try {
-      const res = await fetch(url, { signal });
-      const data = await res.json();
-      setPokemonData(data.results);
-      setNextPageUrl(data.next);
-      setPrevPageUrl(data.previous);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPokemonData();
-    return () => {
-      controller.abort();
-    };
-  }, [url]);
+  function handleSelectPokemon(pokemon: string) {
+    setSelectedPokemon(pokemon);
+  }
 
   return (
-    <div className={styles.container}>
+    <>
       {/* title */}
-      <h1>Pokemon App</h1>
-
-      {/* search pokemon */}
-      <input
-        type="text"
-        className={styles.search}
-        placeholder="Search Pokemon"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
-      {/* pokemon list */}
-      <ul>
-        {pokemonData
-          .filter(({ name }: { name: string }) => name.includes(search))
-          .map(({ name }: { name: string }) => (
-            <li key={name}>{name}</li>
-          ))}
-      </ul>
-
-      {/* pagination buttons */}
-      <div className={styles.buttons}>
-        <button
-          onClick={() => setUrl(prevPageUrl ? prevPageUrl : "")}
-          style={{
-            opacity: prevPageUrl ? 1 : 0.5,
-            cursor: prevPageUrl ? "pointer" : "not-allowed",
-          }}
-          disabled={!prevPageUrl}
-        >
-          previous
-        </button>
-        <button onClick={() => setUrl(nextPageUrl ? nextPageUrl : "")}>
-          next
-        </button>
+      <h1 style={{ textAlign: "center" }}>Pokemon App</h1>
+      <div className={styles.container}>
+        {selectedPokemon ? (
+          <PokemonCard
+            selectedPokemon={selectedPokemon}
+            parentUrl={url}
+            clearHandler={() => setSelectedPokemon(null)}
+          />
+        ) : (
+          <PokemonList
+            url={url}
+            setUrl={setUrl}
+            handleSelectPokemon={handleSelectPokemon}
+          />
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
